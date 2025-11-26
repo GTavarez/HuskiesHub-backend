6aa2d72338e94df7afe8c54d73321c7349248507
GTavarez <Giselltavarez@gmail.com>
2025-11-25 21:48:26 -0500

    schedule fixed

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/auth.js");
 const { JWT_SECRET } = require("../utils/config.js");
+require("dotenv").config();
+
 
 const signup = async (req, res) => {
   try {
@@ -11,47 +13,39 @@ const signup = async (req, res) => {
       return res.status(400).send({ message: "Missing required fields" });
     }
 
-    // Hash password
+    if (password !== confirmPassword) {
+      return res
+        .status(400)
+        .send({ message: "Passwords do not match" });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create user
     const user = await User.create({
       name,
       email,
-      password: hashedPassword,
+      password: hashedPassword, // ONLY ONE PASSWORD
     });
 
-    // Generate JWT
-    const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: "7d" });
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
 
-    // Send response
     res.status(201).send({
       token,
-      user: { _id: user._id, name: user.name, email: user.email },
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
     });
   } catch (err) {
     console.error("Signup error:", err);
     res.status(500).send({ message: "Internal server error" });
   }
 };
-const getCurrentUser = (req, res) => {
-  const { _id: userId } = req.user;
-  return User.findById(userId)
-    .orFail()
-    .then((user) => {
-      res.status(200).send({
-        user: {
-          name: user.name,
-          email: user.email,
-          _id: user._id,
-        },
-      });
-    })
 
-    .catch((error) => {
-      res.status(500).send({ message: error.message });
-    });
-};
+
 const signin = (req, res) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
--- models/auth.js
+++ models/auth.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,27 +24,19 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
 userSchema.statics.findUserByCredentials = async function (email, password) {
   const user = await this.findOne({ email }).select("+password");
-
   if (!user) {
     throw new Error("Incorrect email or password");
   }
-
   const isMatch = await bcrypt.compare(password, user.password);
-
   if (!isMatch) {
     throw new Error("Incorrect email or password");
   }
-
   return user;
 };
 
-userSchema.methods.generateAuthToken = function () {
-  const user = this;
-  const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: "7d" });
-  return token;
-};
 const User = mongoose.model("User", userSchema);
-
 module.exports = User;
+
